69b4c937ae05107a2f3e0bf0a54d9f4f8081cf85
Sandip Thapa <sandipthapa386@gmail.com>
2023-09-13 14:48:16 +0545

    basic file read and write in node

---
+++ index.js
@@ -0,0 +1,28 @@
+const fs = require("fs")
+const path = require("path")
+
+fs.readFile("./files/starter.txt", "utf8", (err,data)=>{
+    // path.join(__dirname, "files", "starter.txt") better way because / may cause issues
+    if(err) throw err;
+    console.log(data)
+})
+
+// write a new file
+// Replaces the file if the filename already exists
+fs.writeFile(path.join(__dirname, "files", "reply.txt"),"Hello world.", (err)=>{
+    if(err) throw err;
+    console.log("Write complete")
+})
+
+// Update/append a file
+// creates a file if it already does not exist
+fs.appendFile(path.join(__dirname, "files", "test.txt"),"This is test file.", (err)=>{
+    if(err) throw err;
+    console.log("Write complete")
+})
+
+// exit on uncaught errors
+process.on("uncaughtException", err=>{
+    console.error(`There was an uncaught error: ${err}`)
+    process.exit(1)
+})
