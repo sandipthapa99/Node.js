276eaa53fbb47854a5e502205892471873f8eb28
Sandip Thapa <sandipthapa386@gmail.com>
2023-09-14 14:55:47 +0545

    NPM modules

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
-const fs = require("fs")
-const path = require("path")
+// const fs = require("fs")
+// const path = require("path")
 
-fs.readFile("./files/starter.txt", "utf8", (err,data)=>{
-    // path.join(__dirname, "files", "starter.txt") better way because / may cause issues
-    if(err) throw err;
-    console.log(data)
-})
+// fs.readFile("./files/starter.txt", "utf8", (err,data)=>{
+//     // path.join(__dirname, "files", "starter.txt") better way because / may cause issues
+//     if(err) throw err;
+//     console.log(data)
+// })
 
-// write a new file
-// Replaces the file if the filename already exists
-fs.writeFile(path.join(__dirname, "files", "reply.txt"),"Hello world.", (err)=>{
-    if(err) throw err;
-    console.log("Write complete")
-})
+// // write a new file
+// // Replaces the file if the filename already exists
+// fs.writeFile(path.join(__dirname, "files", "reply.txt"),"Hello world.", (err)=>{
+//     if(err) throw err;
+//     console.log("Write complete")
+// })
 
-// Update/append a file
-// creates a file if it already does not exist
-fs.appendFile(path.join(__dirname, "files", "test.txt"),"This is test file.", (err)=>{
-    if(err) throw err;
-    console.log("Write complete")
-})
+// // Update/append a file
+// // creates a file if it already does not exist
+// fs.appendFile(path.join(__dirname, "files", "test.txt"),"This is test file.", (err)=>{
+//     if(err) throw err;
+//     console.log("Write complete")
+// })
 
-// exit on uncaught errors
-process.on("uncaughtException", err=>{
-    console.error(`There was an uncaught error: ${err}`)
-    process.exit(1)
-})
+// // exit on uncaught errors
+// process.on("uncaughtException", err=>{
+//     console.error(`There was an uncaught error: ${err}`)
+//     process.exit(1)
+// })
+
+const { format } = require("date-fns")
+const { v4: uuid } = require("uuid")
+
+console.log(format(new Date(), "pp"))
+console.log(uuid()) 
