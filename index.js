e22bc9f671e3a71b44b28f601be67b987a0f2049
Sandip Thapa <sandipthapa386@gmail.com>
2023-09-17 12:49:17 +0545

    Event Emitter

--- index.js
+++ index.js
@@ -1,34 +1,16 @@
-// const fs = require("fs")
-// const path = require("path")
+const logEvents = require("./logEvent")
 
-// fs.readFile("./files/starter.txt", "utf8", (err,data)=>{
-//     // path.join(__dirname, "files", "starter.txt") better way because / may cause issues
-//     if(err) throw err;
-//     console.log(data)
-// })
+const EventEmitter = require("events")
 
-// // write a new file
-// // Replaces the file if the filename already exists
-// fs.writeFile(path.join(__dirname, "files", "reply.txt"),"Hello world.", (err)=>{
-//     if(err) throw err;
-//     console.log("Write complete")
-// })
+class MyEmitter extends EventEmitter { }
 
-// // Update/append a file
-// // creates a file if it already does not exist
-// fs.appendFile(path.join(__dirname, "files", "test.txt"),"This is test file.", (err)=>{
-//     if(err) throw err;
-//     console.log("Write complete")
-// })
+// initialixe object
+const myEmitter = new MyEmitter()
 
-// // exit on uncaught errors
-// process.on("uncaughtException", err=>{
-//     console.error(`There was an uncaught error: ${err}`)
-//     process.exit(1)
-// })
+// add listener for the log event
+myEmitter.on("log", (msg) => logEvents(msg))
 
-const { format } = require("date-fns")
-const { v4: uuid } = require("uuid")
-
-console.log(format(new Date(), "pp"))
-console.log(uuid()) 
+setTimeout(() => {
+    // Emit Event
+    myEmitter.emit("log", "Log event emitted!")
+}, 2000)
---
+++ logEvent.js
@@ -0,0 +1,25 @@
+const { format } = require("date-fns")
+const { v4: uuid } = require("uuid")
+
+const fs = require("fs")
+const fsPromises = require("fs").promises
+const path = require("path")
+
+const logEvents = async (message) => {
+    const dateTime = `${format(new Date(), "pp")}`
+    const logItem = `${dateTime}\t${uuid()}\t${message}`
+    console.log(logItem)
+
+    try {
+        if (!fs.existsSync(path.join(__dirname, "logs"))) {
+            await fsPromises.mkdir(path.join(__dirname, "logs"))
+        }
+        await fsPromises.appendFile(path.join(__dirname, "logs", "events.txt"), logItem)
+
+    } catch (err) {
+        console.error(err)
+
+    }
+}
+
+module.exports = logEvents
